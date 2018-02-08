5907a2c8290ae625cd025b5e524191242804f4e7
Olivia Zhao <mavis.zfy@gmail.com>
2018-02-08 11:22:58 -0600

    part1 Done

--- exercise_3_1.py
+++ exercise_3_1.py
@@ -1,10 +1,19 @@
+'''
+Created on Feb 8, 2018
+
+@author: fangyunzhao
+'''
+
 import numpy as np
 
 def repetition(letters, numberBeforeSwitch, numRepetition):
     for i in range(numRepetition):
         for l in letters:
             list = np.repeat(l, numberBeforeSwitch)
+            print np.repeat(l, numberBeforeSwitch)
             for i in range(len(list)):
-                print list[i]
+               print list[i]
 
 repetition(['A', 'B'], 2, 2)
+    
+
